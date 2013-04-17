f68dcc6971cd72bb06b9d11700005f9198befccf
Samuel Carlsson <samuel.carlsson@gmail.com>
2013-04-17 08:45:19 +0200

    Adding unit test and first test.

---
+++ NGTP.Specs/Given_GtpClient.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace NGTP.Specs
+{
+    [TestFixture]
+    public class Given_GtpClient
+    {
+        [Test]
+        public void TestGetVersion()
+        {
+            var outstream = new MemoryStream();
+            var instream = new MemoryStream();
+            TextWriter output = new StreamWriter(outstream);
+            TextReader input = new StreamReader(instream);
+
+            var mock1 = new StreamWriter(instream);
+            mock1.Write("= 44\n");
+            mock1.Flush();
+            mock1.BaseStream.Seek(0, SeekOrigin.Begin);
+
+            var client = new GtpClient(input, output);
+            var actual = client.GetVersion();
+            Assert.That(actual, Is.EqualTo(44));
+        }
+    }
+}
--- NGTP/GTPClient.cs
+++ NGTP/GTPClient.cs
@@ -1,6 +1,34 @@
-﻿namespace NGTP
+﻿using System;
+using System.IO;
+
+namespace NGTP
 {
-    public class GTPClient
+    public class GtpClient
     {
+        private readonly TextReader _input;
+        private readonly TextWriter _output;
+
+        public GtpClient(TextReader input, TextWriter output)
+        {
+            _input = input;
+            _output = output;
+        }
+
+        public int GetVersion()
+        {
+            _output.WriteLine("protocol_version");
+            var answer = _input.ReadLine();
+            if (answer != null)
+            {
+                var firstSpaceIndex = answer.IndexOf(" ", StringComparison.Ordinal);
+                if (firstSpaceIndex >= 0)
+                {
+                    var a = answer.Substring(0, firstSpaceIndex);
+                    var b = answer.Substring(firstSpaceIndex + 1, answer.Length - firstSpaceIndex - 1);
+                    return int.Parse(b);
+                }
+            }
+            return -1;
+        }
     }
 }
